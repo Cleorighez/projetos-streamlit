a518e9876d25e9d5606ea6b6d9f7b9aeb4b12195
Cleorighez <168569500+Cleorighez@users.noreply.github.com>
2024-09-24 15:57:59 -0300

    Update Consulta.py

--- pages/Consulta.py
+++ pages/Consulta.py
@@ -3,9 +3,31 @@ import pandas as pd
 
 st.set_page_config("Consulta de Clientes", page_icon=("ico LOGO.32x32.Imatik.png"))
 
-dados = pd.read_csv("clientes.csv")
+dados = pd.read_csv('clientes.csv')
 
-st.title("Clientes cadastrados")
+st.title('Consulta de clientes')
+
+busca = st.text_input('Pesquisar')
+
+if busca:
+    # filtra os dados pela coluna "nome"
+    resultados = dados[dados['nome'].str.contains(busca, case=False, na=False)]
+
+    # exibe os resultados em uma tabela
+    if not resultados.empty:
+        st.write('Resultados encontrados:')
+        st.dataframe(resultados)
+    else:
+        st.write('Nenhum resultado encontrado.')
 st.divider()
 
-st.dataframe(dados)
+
+
+
+
+# toggle da tabela
+
+tabela_clientes = st.toggle('Tabela de clientes')
+
+if tabela_clientes:
+    st.dataframe(dados)
