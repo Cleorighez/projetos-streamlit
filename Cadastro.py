43411c08b5638dec794093750864f98316d69616
Cleorighez <168569500+Cleorighez@users.noreply.github.com>
2024-09-24 16:19:15 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -1,46 +1,76 @@
 import streamlit as st
-import pandas as pd 
-from datetime import date
+import pandas as pd
+import datetime
+from datetime import date, datetime
 
-def gravar_dados(nome,dt_nasc,tipo):
-    if nome and dt_nasc <= date.today() and tipo:
-        with open("clientes.csv","a",encoding="utf-8") as file:
-            file.write(f"{nome}, {dt_nasc}, {tipo}\n")
-        st.session_state["Sucesso"] = True
+
+
+
+def gravar_dados(nome, dt_nasc, sexo, est_civil, cpf, data_cadastro):
+    if nome and dt_nasc <= date.today():
+        with open('clientes.csv', 'a', encoding='utf-8') as file:
+            file.write(f'{nome}, {dt_nasc}, {sexo}, {est_civil}, {cpf}, {data_cadastro}\n')
+        st.session_state['Sucesso'] = True
     else:
-        st.session_state["Sucesso"]= False
-    
+        st.session_state['Sucesso'] = False
 
-st.set_page_config("Cadastro de Clientes", page_icon=("ico LOGO.32x32.Imatik.png"))
+st.set_page_config(
+		"Cadastro de Clientes", 
+		page_icon=("ico LOGO.32x32.Imatik.png")
+		)
 
-st.title("Cadastro de clientes")
+st.title('Cadastro de clientes')
 st.divider()
 
-nome = st.text_input("Digite o nome do cliente",
-                     placeholder="Digite o nome.....",
-                     key="nome_cliente"
-                     )
+# nome
+nome = st.text_input('Digite o nome do cliente',
+                     key='nome_cliente')
 
-dt_nasc = st.date_input("data de nascimento",
-                        format= "DD/MM/YYYY",
-                        min_value= date(1950,1,1),
-                        key="data_nasc"
+# data minima e maxima
+min_date = date(1900, 1, 1)  # usando apenas date, não datetime.date
+max_date = date(2100, 12, 31)  # mesmo aqui
+
+# data de nascimento
+dt_nasc = st.date_input('Data de nascimento',
+                        value=date(2023,1,1),
+                        min_value=min_date,
+                        max_value=max_date
                         )
 
-tipo = st.selectbox("Escolha o tipo de cliente",
-                    ["Pessoa Juridica", "Pessoa física"],
-                    index=None,
-                    placeholder="Escolha a Tipologia.....",
-                    key="tipologia"
-                    )
+# sexo
+sexo = st.selectbox('Sexo:',
+                    ['F', 'M'])
 
-btn_cadastro = st.button("Cadastrar", 
-                         on_click= gravar_dados,
-                         args=[nome,dt_nasc,tipo])
+# estado civil
+est_civil = st.selectbox('Estado civil:',
+                         ['Solteiro(a)', 'Casado(a)', 'Divorciado(a)', 'Viúvo(a)'])
 
-if btn_cadastro:
-    if st.session_state["Sucesso"]:
-        st.success("Dados cadastrados com sucesso", icon="🆗")
+# cpf
+cpf = st.text_input('CPF', key='cpf')
+
+# data atual
+data_cadastro = datetime.today().date()
+
+# adicionar um input com a data atual como padrão:
+data_selecionada = st.date_input('Data de cadastro:', data_cadastro)
+
+# botao para salvar dados
+btn_cadastrar = st.button('Cadastrar',
+                          on_click=gravar_dados,
+                          args=[nome,dt_nasc,sexo,est_civil,cpf,data_cadastro])
+
+# Botão de cadastro
+if btn_cadastrar:
+    # Verifica se todos os campos foram preenchidos
+    if not nome or not cpf:
+        st.error("❌❌ Por favor, preencha todos os campos obrigatórios.")
     else:
-        st.error("Houve algum erro no cadastro",
-                 icon="❌")
+        # Mostra a mensagem de sucesso com os dados cadastrados
+        st.success(f"❎Usuário cadastrado com sucesso!")
+        st.write("### Dados do Usuário:")
+        st.write(f"**Nome:** {nome}")
+        st.write(f"**Sexo:** {sexo}")
+        st.write(f"**Estado Civil:** {est_civil}")
+        st.write(f"**CPF:** {cpf}")
+        st.write(f"**Data de Nascimento:** {dt_nasc}")
+        st.write(f"**Data de Cadastro:** {data_cadastro}")
